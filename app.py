f519945f689bb96178be3f9a462a98f742e36b01
Johnson Hsieh <tengors888@gmail.com>
2024-01-27 14:33:11 +0800

    update textMessage

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 configuration = Configuration(os.environ.get('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
-
+line_bot_api = MessagingApi(ApiClient(configuration))
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -31,15 +31,22 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
+    line_bot_api.reply_message_with_http_info(
+        ReplyMessageRequest(
+            reply_token=event.reply_token,
+            messages=[TextMessage(text=event.message.text)]
         )
+    )
 
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        ReplyMessageRequest(
+            reply_token=event.reply_token,
+            messages=[TextMessage(text=event.message.text)]
+        )
+    )
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
