09bb75e2e8641cc1b844ff7f43085f65310a6ce1
Johnson Hsieh <tengors888@gmail.com>
2024-01-28 13:48:23 +0800

    update for app run

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event, event.reply_token, TextMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token, TextMessage(text=event.message.text))
     # line_bot_api.reply_message(
     #     ReplyMessageRequest(
     #         reply_token=event.reply_token,
@@ -39,5 +39,6 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
