c5ed0254e6438798f7df75ff826cf44f20da7c2f
Johnson Hsieh <tengors888@gmail.com>
2024-01-29 22:17:09 +0800

    test3

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import os
-import sys
-from argparse import ArgumentParser
 from flask import Flask, request, abort
 from linebot.v3 import (WebhookHandler)
 from linebot.v3.exceptions import (InvalidSignatureError)
-from linebot.v3.messaging import (MessagingApi, ReplyMessageRequest, TextMessage)
+from linebot.v3.messaging import (Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage)
 from linebot.v3.webhooks import (MessageEvent, TextMessageContent)
 
 
 app = Flask(__name__)
 
+configuration = Configuration(access_token=os.environ.get('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
-line_bot_api = MessagingApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
+
 
 @app.route("/")
 def index():
@@ -34,15 +33,16 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextMessage(text='ABC123'))#event.message.text
-    # line_bot_api.reply_message(
-    #     ReplyMessageRequest(
-    #         reply_token=event.reply_token,
-    #         messages=[TextMessage(text=event.message.text)]
-    #     )
-    # )
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
 
 
 if __name__ == "__main__":
